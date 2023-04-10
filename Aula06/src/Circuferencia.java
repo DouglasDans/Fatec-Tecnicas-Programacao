d1ffab9ca2e75f1df2c06785cf41467290ddfb49
DouglasDans <douglas.dans@gmail.com>
2023-04-10 15:52:55 -0300

    Aula06

---
+++ Aula06/src/Circuferencia.java
@@ -0,0 +1,30 @@
+public class Circuferencia extends Forma{
+    private int raio = 0;
+
+    public Circuferencia(int raio) {
+        this.raio = raio;
+    }
+
+    public int getCircuferencia() {
+        return raio;
+    }
+
+    public void setCircuferencia(int raio) {
+        this.raio = raio;
+    }
+
+    public float area(){
+
+        return 0.0F;
+    }
+
+    public float perimetro(){
+
+        return 0.0F;
+    }
+
+
+    public void show(){
+
+    }
+}
---
+++ Aula06/src/Forma.java
@@ -0,0 +1,5 @@
+public class Forma {
+    public float area(){return 0.0F;}
+    public void show(){};
+    public float perimetro() {return 0.0F;}
+}
