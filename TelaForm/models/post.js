3b93c36d713706678cabb7b45b1e053d081c41e7
Douglas Nascimento <56117204+DouglasDans@users.noreply.github.com>
2023-04-19 11:14:13 -0300

    Add files via upload

---
+++ TelaForm/models/post.js
@@ -0,0 +1,22 @@
+const db = require("./banco")
+
+const Agendamentos = db.sequelize.define("agendamentos" , {
+    nome:{
+        type: db.Sequelize.STRING
+    },
+    telefone: {
+        type: db.Sequelize.SMALLINT
+    },
+    origem:{
+        type: db.Sequelize.SMALLINT
+    },
+    data: {
+        type: db.Sequelize.STRING
+    },
+    observacao: {
+        type: db.Sequelize.STRING
+    },
+})
+// Agendamentos.sync()
+
+module.exports = Agendamentos
